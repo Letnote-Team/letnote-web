2f334009e3ba60a132555199a8562346459d7b54
Letnote Team <letnote.enterprise@gmail.com>
2022-11-18 09:55:39 -0300

    Adicionado botão para criar novas anotações

--- src/components/layouts/editorLayout.tsx
+++ src/components/layouts/editorLayout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { useTree } from "../../hooks/useTree";
 import { Logo } from "../commons/Logo";
 import { Tree } from "../Tree/Tree";
+import { Button } from "../commons/Button";
 
 const TreeLayout = ({ children }: { children?: ReactNode }) => {
   const { tree } = useTree();
@@ -9,13 +10,16 @@ const TreeLayout = ({ children }: { children?: ReactNode }) => {
   return (
     <div className="flex">
       <div
-        className="w-64 p-4 bg-neutral-50 h-screen fixed overflow-hidden"
+        className="w-64 xl:w-80 p-4 bg-neutral-50 h-screen fixed top-0 bottom-0 overflow-hidden"
         aria-label="Sidebar"
       >
         <Logo />
-        <div className="mt-8 space-y-1">
-          <h1 className="font-bold">ANOTAÇÕES</h1>
-          <div className="-ml-1">{tree && <Tree tree={tree} />}</div>
+        <div className="flex justify-between flex-col h-[calc(100%-36px)]">
+          <div className="mt-8 flex gap-1">
+            <h1 className="font-bold">ANOTAÇÕES</h1>
+            <div className="-ml-1">{tree && <Tree tree={tree} />}</div>
+          </div>
+          <Button className="mt-auto w-full">Criar</Button>
         </div>
       </div>
       <div className="flex-1 ml-64">{children}</div>
